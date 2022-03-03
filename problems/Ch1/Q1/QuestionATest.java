ce4e5000c08796b435e8a07d324b9a1c0d6d7189
zackary-clark <zackary.a.clark@gmail.com>
2022-03-02 22:31:06 -0500

    Move files around into solutions & problems

---
+++ problems/Ch1/Q1/QuestionATest.java
@@ -0,0 +1,12 @@
+package Q1;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class QuestionATest {
+    @Test
+    public void testName() {
+        assertTrue(false);
+    }
+}
